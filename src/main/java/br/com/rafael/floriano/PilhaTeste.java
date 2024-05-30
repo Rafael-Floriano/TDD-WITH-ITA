1591956de42e858e395dae8e249bce2e778d90f7
rafael <rafael.florianoc9@gmail.com>
2024-05-30 13:26:10 -0300

    TESTS: created new test 'empilhaUmElemento' in 'PilhaTeste'

--- src/main/java/br/com/rafael/floriano/PilhaTeste.java
+++ src/main/java/br/com/rafael/floriano/PilhaTeste.java
@@ -2,8 +2,7 @@ package br.com.rafael.floriano;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PilhaTeste {
 
@@ -12,6 +11,15 @@ public class PilhaTeste {
         Pilha p = new Pilha();
         assertTrue(p.isEmpty());
         assertEquals(0, p.size());
+    }
+
+    @Test
+    void empilhaUmElemento() {
+        Pilha p = new Pilha();
+        p.empilha("Primeiro elemento");
+        assertFalse(p.isEmpty());
+        assertEquals(1, p.size());
+        assertEquals("Primeiro elemento", p.topo());
     }
 
 }
