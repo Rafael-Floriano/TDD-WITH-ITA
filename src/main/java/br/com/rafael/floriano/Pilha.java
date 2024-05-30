77af58e2785d29e37a71913f7278ff3cd92316bc
rafael <rafael.florianoc9@gmail.com>
2024-05-30 13:26:45 -0300

    FEAT: implemented new method 'empilha' in object 'Pilha'

--- src/main/java/br/com/rafael/floriano/Pilha.java
+++ src/main/java/br/com/rafael/floriano/Pilha.java
@@ -2,12 +2,23 @@ package br.com.rafael.floriano;
 
 public class Pilha {
 
+    private Object elemento;
+    private int quantidade;
+
     public boolean isEmpty() {
-        return true;
+        return elemento == null;
     }
 
     public int size() {
-        return 0;
+        return quantidade;
     }
 
+    public void empilha(Object elemento) {
+        quantidade++;
+        this.elemento = elemento;
+    }
+
+    public Object topo() {
+        return elemento;
+    }
 }
